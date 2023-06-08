87c277e5a8bfeefc0e81c7a8a66c541c534d20e5
GioMart09 <119916738+GioMart09@users.noreply.github.com>
2023-06-08 19:38:53 -0400

    win condition

--- index.js
+++ index.js
@@ -35,4 +35,22 @@ function reset() {
     nextLetter = LETTER_X
 };
 
+const winCondition = [
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+    [0,4,8]
+    [2,4,6]
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+]
+
+function winConditionMet() {
+    
+}
+
+
+
+
 beginGame();
